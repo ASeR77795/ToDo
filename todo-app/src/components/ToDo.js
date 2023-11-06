9ea460a7c08ff216d825fbbf84ca14de6a66b79f
Pavlo <winterspray@gmail.com>
2023-11-05 19:44:45 -0500

    create delete btn

--- todo-app/src/components/ToDo.js
+++ todo-app/src/components/ToDo.js
@@ -27,7 +27,7 @@ const ToDo = () => {
 				>
 					<div className='mb-3'>
 						<label className='form-label' htmlFor='titleInput'>
-							Title
+							Todo List
 						</label>
 						<input
 							className={`form-control ${!!error ? 'is-invalid' : ''}`}
@@ -41,14 +41,64 @@ const ToDo = () => {
 						/>
 						{!!error && <div className='invalid-feedback'>{error}</div>}
 					</div>
-					<button className='btn btn-primary w-100' type='submit'>
+					<button className='btn btn-primary w-100 mb-2' type='submit'>
 						Create
 					</button>
 				</form>
 			</div>
-			<ul>
+			<ul
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					gap: 10,
+				}}
+			>
 				{todos.map(({ id, isCompleted, title }) => {
-					return <li key={id}>{title}</li>;
+					return (
+						<li
+							style={{
+								display: 'flex',
+								justifyContent: 'space-between',
+							}}
+							key={id}
+							className={isCompleted ? 'text-decoration-line-through' : ''}
+						>
+							{title}
+							<div>
+								<button
+									className='btn btn-secondary'
+									onClick={() => {
+										setTodos(prevState => {
+											return prevState.map(todo => {
+												if (todo.id === id) {
+													return {
+														...todo,
+														isCompleted: !todo.isCompleted,
+													};
+												}
+												return todo;
+											});
+										});
+									}}
+								>
+									Complete
+								</button>
+								<button
+									className='btn btn-danger ms-1'
+									onClick={() => {
+										setTodos(prevState => {
+											const updateTodos = prevState.filter(todo => {
+												return todo.id !== id;
+											});
+											return updateTodos;
+										});
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
